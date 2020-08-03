f1dd7ce6274d79fd5fbbaea6adcf6dcc627d0852
bishal9861 <69111911+bishal9861@users.noreply.github.com>
2020-08-03 09:04:44 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,31 @@
+
+
+#1: write different list
+listofnumber=[1,2,4,5,6,7,8,8,9,7]
+print(listofnumber)
+listofname=["bishal","bubun","banti","chuni"]
+print(listofname)
+listofboth=[1,3,"name","book"]
+print(listofboth)
+
+  #2:accesing element from tuple
+tupleof_number=(1,2,3,4)
+print(tupleof_number)
+#access element
+print(tupleof_number[0])
+print(tupleof_number[1])
+print(tupleof_number[2])
+print(tupleof_number[3])
+
+
+
+#3:del item from dictionary
+dictionary={"name":"bishal","title":"patel","rollnumber":12}
+print(dictionary)
+#accent key value with name
+print(dictionary["name"])
+# del item with key name
+del dictionary["name"]
+print(dictionary)
+
+
